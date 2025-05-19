c024a879a3919b3d2e8ad0ae23abe4faf80da804
Josue Medina Guerrero <josuemedina.032810@gmail.com>
2025-05-19 09:52:14 -0500

    first commit

---
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:teslo_shop/features/shared/shared.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final scaffoldBackgroundColor = Theme.of(context).scaffoldBackgroundColor;
+
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+          body: GeometricalBackground(
+              child: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 80, // todo: aquí iba 80 que colocó Fernando originalmente
+            ),
+            // Icon Banner
+            const Icon(
+              Icons.production_quantity_limits_rounded,
+              color: Colors.white,
+              size: 100,
+            ),
+            const SizedBox(height: 80),
+
+            Container(
+              height: size.height - 260, // 80 los dos sizebox y 100 el ícono
+              decoration: BoxDecoration(
+                color: scaffoldBackgroundColor,
+                borderRadius:
+                    const BorderRadius.only(topLeft: Radius.circular(100)),
+              ),
+              child: const _LoginForm(),
+            )
+          ],
+        ),
+      ))),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm();
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text('Login', style: textStyles.titleLarge),
+          const CustomTextFormField(
+            label: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+          ),
+          const CustomTextFormField(
+            label: 'Contraseña',
+            obscureText: true,
+          ),
+          SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: CustomFilledButton(
+                text: 'Ingresar',
+                buttonColor: Colors.black,
+                onPressed: () {},
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('¿No tienes cuenta?'),
+              TextButton(
+                  onPressed: () => context.push('/register'),
+                  child: const Text('Crea una aquí'))
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
