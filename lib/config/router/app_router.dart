83fbf0ca9f368edbf5e3b01c652a4755ce84b363
Josue Medina Guerrero <josuemedina.032810@gmail.com>
2025-05-23 21:12:28 -0500

    cierre de sesion

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,21 +1,14 @@
-import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
+import 'package:go_router/go_router.dart';
+
 final appRouter = GoRouter(
-  initialLocation: '/login',
+  initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
+    GoRoute(path: '/splash', builder: (context, state) => const CheckAuthScreen()),
+    GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+    GoRoute(path: '/register', builder: (context, state) => const RegisterScreen()),
+    GoRoute(path: '/', builder: (context, state) => const ProductsScreen()),
   ],
 );
--- lib/features/auth/presentation/screens/screens.dart
+++ lib/features/auth/presentation/screens/screens.dart
@@ -1,2 +1,3 @@
-export 'login_screen.dart';
-export 'register_screen.dart';
+export 'package:teslo_shop/features/auth/presentation/screens/check_auth_screen.dart';
+export 'package:teslo_shop/features/auth/presentation/screens/login_screen.dart';
+export 'package:teslo_shop/features/auth/presentation/screens/register_screen.dart';
